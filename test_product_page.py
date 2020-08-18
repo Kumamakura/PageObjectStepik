2584a7b689158ea5dd9052a857009f4bab899a20
Kumamakura <zalender@gmail.com>
2020-08-18 21:07:54 +0500

    Подготовила код к ревью по заданию https://stepik.org/lesson/201964/step/14?unit=176022

--- test_product_page.py
+++ test_product_page.py
@@ -4,7 +4,7 @@ from .pages.basket_page import BasketPage
 from .pages.product_page import ProductPage
 from .pages.login_page import LoginPage
 
-link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+link_product = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
 link_login = "http://selenium1py.pythonanywhere.com/ru/accounts/login"
 
 
@@ -18,42 +18,69 @@ class TestUserAddToBasketFromProductPage:
         page_login.should_be_authorized_user()
 
     def test_user_cant_see_success_message(self, browser):
-        page = ProductPage(browser, link)
+        page = ProductPage(browser, link_product)
         page.open()
         page.should_not_be_success_message()
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
-        page = ProductPage(browser, link)
+        page = ProductPage(browser, link_product)
         page.open()
         page.add_to_basket()
         page.should_be_correct_product_price_in_basket()
         page.should_be_correct_product_in_basket()
 
 
+@pytest.mark.need_review
+def test_guest_can_add_product_to_basket(browser):
+    page = ProductPage(browser, link_product)
+    page.open()
+    page.add_to_basket()
+    page.should_be_correct_product_price_in_basket()
+    page.should_be_correct_product_in_basket()
+
+
+@pytest.mark.need_review
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    page = ProductPage(browser, link_product)
+    page.open()
+    page.go_to_basket()
+    basket = BasketPage(browser, browser.current_url)
+    basket.should_not_be_basket_items()
+    basket.should_be_empty_basket_text()
+
+
+@pytest.mark.need_review
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    page = ProductPage(browser, link_product)
+    page.open()
+    page.should_be_login_link()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
+
+
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
-    page = ProductPage(browser, link)
+    page = ProductPage(browser, link_product)
     page.open()
     page.add_to_basket()
     page.should_not_be_success_message()
 
 
 def test_guest_cant_see_success_message(browser):
-    page = ProductPage(browser, link)
+    page = ProductPage(browser, link_product)
     page.open()
     page.should_not_be_success_message()
 
 
 def test_message_disappeared_after_adding_product_to_basket(browser):
-    page = ProductPage(browser, link)
+    page = ProductPage(browser, link_product)
     page.open()
     page.add_to_basket()
     page.should_disappear_success_message()
 
 
-def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
-    page = ProductPage(browser, link)
+def test_guest_should_see_login_link_on_product_page(browser):
+    page = ProductPage(browser, link_product)
     page.open()
-    page.go_to_basket()
-    basket = BasketPage(browser, browser.current_url)
-    basket.should_not_be_basket_items()
-    basket.should_be_empty_basket_text()
+    page.should_be_login_link()
